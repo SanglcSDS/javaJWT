9b31266710e87b97cf7807c3e34e7974d7289cac
sanglcsds <sanglc@mitec.com.vn>
2021-12-21 17:34:11 +0700

    sanglc

---
+++ src/main/java/com/Project/ProjectSpringBoot/ServletInitializer.java
@@ -0,0 +1,14 @@
+/*
+ * package com.Project.ProjectSpringBoot;
+ * 
+ * import org.springframework.boot.builder.SpringApplicationBuilder; import
+ * org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+ * 
+ * public class ServletInitializer extends SpringBootServletInitializer {
+ * 
+ * @Override protected SpringApplicationBuilder
+ * configure(SpringApplicationBuilder application) { return
+ * application.sources(ProjectSpringBootApplication.class); }
+ * 
+ * }
+ */
